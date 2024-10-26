d715a47e87846a36f7fdf794cac5db0eb2d437df
Raphael <raphael.ezeigwe@yahoo.com>
2024-10-26 23:08:28 +0100

    Fixed Suspense problem

--- app/page.js
+++ app/page.js
@@ -4,15 +4,20 @@ import Categories from "./components/categories";
 import Products from "./components/products";
 import Footer from "./components/footer";
 import OrderDialog from "./components/orderDialog";
+import { Suspense } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <Nav />
-      <Header />
-      <Categories />
-      <Products />
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Nav />
+            <Header />
+            <Categories />
+            <div>
+                <Suspense>
+                    <Products />
+                </Suspense>
+            </div>
+            <Footer />
+        </div>
+    );
 }
