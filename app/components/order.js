56ee83d9864f21f94057501431b417f17c210676
Raphael <raphael.ezeigwe@yahoo.com>
2024-11-09 06:36:23 +0100

    Handle cancel order button

--- app/components/order.js
+++ app/components/order.js
@@ -5,9 +5,12 @@ import { useEffect, useState, useRef } from "react";
 export default function Order({ isOrderOpen, setIsOrderOpen, currProduct }) {
     const sizesRef = useRef(null);
     const coloursRef = useRef(null);
+    const [colError, setColError] = useState(false)
+    const [sizeError, setSizeError] = useState(false)
+    const [colSize, setColSize] = useState({})
 
     const handleClick = (event, classname) => {
-        let container = classname == "colours" ? coloursRef.current : sizesRef.current;
+        let container = classname == "colour" ? coloursRef.current : sizesRef.current;
         container = container.getElementsByClassName(classname);
 
         for (const elem of container) {
@@ -15,6 +18,7 @@ export default function Order({ isOrderOpen, setIsOrderOpen, currProduct }) {
         }
 
         event.target.classList.add("active");
+        setColSize({...colSize, [classname]: event.target.textContent})
     };
 
     useEffect(() => {
@@ -27,9 +31,41 @@ export default function Order({ isOrderOpen, setIsOrderOpen, currProduct }) {
         }
     });
 
+    const handleAddToCart = () => {
+        if (currProduct && currProduct.colours.length && !("colour" in colSize)) {
+            setColError(true)
+        }
+
+        if (currProduct && currProduct.sizes.length && !("size" in colSize)) {
+            setSizeError(true)
+        }
+
+        // Handle other order processes
+    }
+
+    const handleCancel = () => {
+        let sizes = sizesRef.current
+        let colours = coloursRef.current
+
+        sizes = sizes.getElementsByClassName("active")
+        
+        for (const s of sizes) {
+            s.classList.remove("active")
+        }
+
+        colours = colours.getElementsByClassName("active")
+
+        for (const c of colours) {
+            c.classList.remove("active")
+        }
+
+        setIsOrderOpen(false);
+        setColError(false)
+        setSizeError(false)
+    }
 
     return (
-        <dialog id="order" className="border border-oxfordblue  rounded-sm z-10 p-4 w-[400px]">
+        <dialog id="order" className="border border-oxfordblue rounded-sm z-10 p-4 w-[400px]">
             <div>
                 <small className="text-md">{currProduct && currProduct.category}</small>
                 <h1 className="uppercase">{currProduct && currProduct.name}</h1>
@@ -37,47 +73,52 @@ export default function Order({ isOrderOpen, setIsOrderOpen, currProduct }) {
             </div>
 
             <div className="mt-4">
-                <h3 className="mb-1">Available Sizes</h3>
+                {
+                    sizeError && <p className="text-red-500">Please select your size</p>
+                }
+                
+                <h3 className="mb-1 font-semibold">Available Sizes</h3>
                 <div ref={sizesRef} className="flex gap-1 flex-wrap">
                     {currProduct && currProduct.sizes.length ? (
                         currProduct.sizes.map((size, index) => (
                             <button
                                 onClick={(event) => {
-                                    handleClick(event, "sizes");
+                                    handleClick(event, "size");
                                 }} 
                                 key={index}
-                                className="sizes px-4 py-2 rounded-sm bg-gray-300 border border-gray-300">{size}</button>
+                                className="size px-4 py-2 rounded-sm bg-gray-300 border border-gray-300">{size}</button>
                         ))
                     ) : (
-                        <div className="sizes px-4 py-2 rounded-sm bg-gray-300 border border-gray-300">N/A</div>
+                        <div className="size px-4 py-2 rounded-sm bg-gray-300 border border-gray-300">N/A</div>
                     )}
                 </div>
             </div>
 
             <div className="mt-4">
-                <h3 className="mb-1">Available Colours</h3>
+                {
+                    colError && <p className="text-red-500">Please select your preferred colour</p>
+                }
+                <h3 className="mb-1 font-semibold">Available Colours</h3>
                 <div ref={coloursRef} className="flex gap-1 flex-wrap">
                     {currProduct && currProduct.colours.length ? (
                         currProduct.colours.map((colour, index) => (
                             <button
                                 onClick={(event) => {
-                                    handleClick(event, "colours");
+                                    handleClick(event, "colour");
                                 }}
                                 key={index}
-                                className="colours px-4 py-2 rounded-sm bg-gray-300 border border-gray-300">{colour}</button>
+                                className="colour px-4 py-2 rounded-sm bg-gray-300 border border-gray-300">{colour}</button>
                         ))
                     ) : (
-                        <div className="colours px-4 py-2 rounded-sm bg-gray-300 border border-gray-300">N/A</div>
+                        <div className="colour px-4 py-2 rounded-sm bg-gray-300 border border-gray-300">N/A</div>
                     )}
                 </div>
             </div>
 
             <div className="w-full flex gap-1 mt-4">
-                <button className="border border-oxfordblue bg-oxfordblue text-white px-4 py-2 rounded-sm w-1/2">Add To Cart</button>
+                <button onClick={handleAddToCart} className="border border-oxfordblue bg-oxfordblue text-white px-4 py-2 rounded-sm w-1/2">Add To Cart</button>
                 <button
-                    onClick={() => {
-                        setIsOrderOpen(false);
-                    }}
+                    onClick={handleCancel}
                     className="border border-oxfordblue px-4 py-2 rounded-sm w-1/2"
                 >
                     Cancel
